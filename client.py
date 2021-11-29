ac6ab64502a6b3670925a64bf4abfac3c0779210
baptiste <baptiste.heraud@epitech.eu>
2021-11-29 14:38:47 +0100

    client close socket.

--- client.py
+++ client.py
@@ -7,9 +7,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 while True :
     command = input("Enter your command :\n")
+    if command.upper() == "exit".upper():
+        break
+    print("[+] send " + command)
     s.send(command.encode())
-    print("Command send")
-    print("en attente de reception")
     data = s.recv(15000)
     print(data.decode())
-
+s.close()
